666518177fa0958494ca52cb6e1ddffef7c5ba46
Kartikey Jaiswal <kartikeyjaiswal@Kartikeys-MacBook-Pro.local>
2023-07-30 20:25:08 +0530

    added redux

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieListing from '../MovieListing/MovieListing';
+import movieApi from '../../common/apis/movieApi';
+import {API_KEY} from '../../common/apis/movieApiKey' ;
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
+    const movieText = 'Avengers';
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const fetchMovies =  async() => {
+            const response = await movieApi
+            .get(`?apiKey=${API_KEY}&s=${movieText}&type=movie`)
+            .catch((err) => {
+                console.log("Err : ", err);
+            });
+            console.log("Response :: ", response);
+            dispatch(addMovies(response.data));
+        };
+        fetchMovies();
+    }, []);
     return (
         <div>
-            Home
+            <div className='banner-img'></div>
+            <MovieListing />
         </div>
     );
 };
