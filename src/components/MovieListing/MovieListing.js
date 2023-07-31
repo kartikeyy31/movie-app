2387dda6fdbc62e3493ede1d0db5e3c68e217ea5
Kartikey Jaiswal <kartikeyjaiswal@Kartikeys-MacBook-Pro.local>
2023-07-31 21:35:49 +0530

    added fetchShows functionality

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { getAllMovies } from '../../features/movies/movieSlice';
+import { getAllMovies, getAllShows } from '../../features/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard';
 import "./MovieListing.scss";
 
 const MovieListing = () => {
     const movies = useSelector(getAllMovies);
-    console.log(movies);
-    let renderMovies = "";
+    const shows = useSelector(getAllShows);
+    // console.log(movies);
+    let renderMovies, renderShows = "";
+
     renderMovies = movies.Response === 'True' ? (
         movies.Search.map((movie, index) => {
             return <MovieCard key={index} data={movie} />
@@ -17,12 +19,28 @@ const MovieListing = () => {
             <h3> {movies.Error} </h3>
         </div>
     );
+
+    renderShows = shows.Response === 'True' ? (
+        shows.Search.map((movie, index) => {
+            return <MovieCard key={index} data={movie} />
+        })
+    ) : (
+        <div className='shows-error'>
+            <h3> {movies.Error} </h3>
+        </div>
+    );
     return (
         <div className='movies-wrapper'>
-            <div className='movies-list'>
+            <div className='movie-list'>
                 <h2>Movies</h2>
                 <div className='movie-container'>
                     {renderMovies}
+                </div>
+            </div>
+            <div className='show-list'>
+                <h2>Shows</h2>
+                <div className='movie-container'>
+                    {renderShows}
                 </div>
             </div>
         </div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -11,8 +11,18 @@ export const fetchAsyncMovies = createAsyncThunk(
     }
 );
 
+export const fetchAsyncShows = createAsyncThunk(
+    'movies/fetchAsyncShows',
+    async () => {
+        const searchText = 'Friends';
+        const response = await movieApi.get(`?apiKey=${API_KEY}&s=${searchText}&type=series`);
+        return response.data;
+    }
+);
+
 const initialState = {
     movies: {},
+    shows: {},
 };
 
 const movieSlice = createSlice({
@@ -28,15 +38,20 @@ const movieSlice = createSlice({
             console.log("Pending");
         },
         [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
-            console.log("Fetched Successfully!");
+            console.log("Fetched Movies Successfully!");
             return { ...state, movies: payload };
         },
         [fetchAsyncMovies.rejected]: () => {
             console.log("Rejected!");
+        },
+        [fetchAsyncShows.fulfilled]: (state, { payload }) => {
+            console.log("Fetched Shows Successfully!");
+            return { ...state, shows: payload };
         },
     },
 });
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
 export default movieSlice.reducer;
